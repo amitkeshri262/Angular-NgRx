4b8ba92e864a6059aaaff7ea574a27c889bc8f6d
amitkeshri262 <amitkeshri262@gmail.com>
2026-03-19 18:11:44 +0530

    Ngrx Concept
    my first Angular App

---
+++ Angular_NgRx/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeaderComponent } from './header/header.component';
+import { CounterValueComponent } from './counter/counter-value/counter-value.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    CounterComponent,
+    HeaderComponent,
+    CounterValueComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
